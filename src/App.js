e0952862f63cd3523942f373f5ec4eabf8080893
Hackyeahh <henry.xiu@gx.cds.on.ca>
2021-07-01 00:14:11 -0400

    name section skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Name />
   );
 }
+
+
+// sections
+
+// components
+function Name(){
+  return (
+    <section class='column center-main name'>
+      <h2 class='name'>Hi, I'm</h2>
+      <h1 class='name'>Henry Xiu.</h1>
+      <p class='name'>Robotics Enthusiast. Competitve Programmer. Future Computer Scientist.</p>
+    </section>
+  );
+}
+
 
 export default App;
